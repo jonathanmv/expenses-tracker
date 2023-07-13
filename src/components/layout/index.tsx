f115cb8d74b1e195decf431de6e56dd9b41c7d22
jonathanmv <jonathanmv@outlook.com>
2023-07-13 07:12:39 +0200

    add layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,37 +1,71 @@
-import { AppShell, Navbar } from "@mantine/core";
-import { useViewportSize } from "@mantine/hooks";
+import {
+  AppShell,
+  ColorSchemeProvider,
+  MantineProvider,
+  Navbar,
+  type ColorScheme,
+} from "@mantine/core";
+import { useColorScheme, useViewportSize } from "@mantine/hooks";
+import { useState } from "react";
 import { Brand } from "./_brand";
 import { MainLinks } from "./_mainLinks";
 import { User } from "./_user";
 
-export default function AddExpensePage() {
+export default function Layout({ children }: { children: React.ReactNode }) {
   const { height } = useViewportSize();
 
   return (
-    <AppShell
-      padding="md"
-      fixed={false}
-      navbar={
-        <Navbar width={{ base: 300 }} height={height - 60} p="xs">
-          <Navbar.Section grow mt="xs">
-            <MainLinks />
-          </Navbar.Section>
-          <Navbar.Section>
-            <User />
-          </Navbar.Section>
-        </Navbar>
-      }
-      header={<Brand />}
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      })}
-    >
-      Your content here
-    </AppShell>
+    <UI>
+      <AppShell
+        padding="md"
+        fixed={false}
+        navbar={
+          <Navbar width={{ base: 300 }} height={height - 60} p="xs">
+            <Navbar.Section grow mt="xs">
+              <MainLinks />
+            </Navbar.Section>
+            <Navbar.Section>
+              <User />
+            </Navbar.Section>
+          </Navbar>
+        }
+        header={<Brand />}
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        {children}
+      </AppShell>
+    </UI>
   );
 }
+
+const UI: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // hook will return either 'dark' or 'light' on client
+  // and always 'light' during ssr as window.matchMedia is not available
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        {children}
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,10 @@
+import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import { api } from "@/utils/api";
-import {
-  ColorSchemeProvider,
-  MantineProvider,
-  type ColorScheme,
-} from "@mantine/core";
-import { useColorScheme } from "@mantine/hooks";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import Head from "next/head";
-import React, { useState, type ReactNode } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -26,37 +20,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <UI>
+      <Layout>
         <SessionProvider session={session}>
           <Component {...pageProps} />
         </SessionProvider>
-      </UI>
+      </Layout>
     </>
-  );
-};
-
-const UI: React.FC<{ children: ReactNode }> = ({ children }) => {
-  // hook will return either 'dark' or 'light' on client
-  // and always 'light' during ssr as window.matchMedia is not available
-  const preferredColorScheme = useColorScheme();
-  const [colorScheme, setColorScheme] =
-    useState<ColorScheme>(preferredColorScheme);
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
-  return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{ colorScheme }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        {children}
-      </MantineProvider>
-    </ColorSchemeProvider>
   );
 };
 
